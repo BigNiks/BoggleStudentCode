d2e5fbf12142b90d29fbea9d065aa5f54c89c4dd
BigNiks <niko.madriz@menloschool.org>
2025-01-13 10:00:54 -0800

    upd

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date |     Time      |                                                                                                                                                                                                                                                                                                  Update |
+|:-----|:-------------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 1/7  |    2-3 PM     |                                                           I decided that I'm gonna try to first approach this problem using BFS, I also thought of using a stack instead of a queue after talking it through with some of my table mates. Today's class was spent reviewing and coding out my BFS code. |
+| 1/9  | 12:45-1:45 PM | Today Mr. Blick explained how instead of using BFS, it would be more efficient to use DFS. Not only that, but instead of trying to implement DFS iteratively, we would try to implement it recursively. Because of this, I have to rewrite most of my code to accommodate this new recursive algorithm. |
+| 1/11 |    3-4 PM     |                                                                                                                             I spent today polishing and refining my recursive algorithm, I also wrote out my copied array to keep track of visited squares. I did this by using a 2d array of booleans. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Boggle.java
+++ src/Boggle.java
@@ -4,16 +4,58 @@ import java.util.Arrays;
 public class Boggle {
 
     public static String[] findWords(char[][] board, String[] dictionary) {
-
+        TST dict = new TST();
         ArrayList<String> goodWords = new ArrayList<String>();
+        boolean[][] copy = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                copy[i][j] = false;
+            }
+        }
+
+        int k = 0;
+        while (k < dictionary.length) {
+            dict.insert(dictionary[k], k);
+        }
 
         // TODO: Complete the function findWords(). Add all words that are found both on the board
         //  and in the dictionary.
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                String temp = "";
+                dfs(temp, board, copy, i, j, goodWords);
+            }
+        }
+
+        for (int i = 0; i < goodWords.size(); i++) {
+            if (dict.lookup(goodWords.get(i)) == -1) {
+                goodWords.remove(i);
+            }
+        }
 
         // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
+    }
+
+    public static void dfs(String prefix, char[][] grid, boolean[][] copy, int i, int j, ArrayList<String> words) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+            words.add(prefix);
+            return;
+        }
+        if (copy[i][j] == true) {
+            words.add(prefix);
+            return;
+        }
+        copy[i][j] = true;
+        prefix += grid[i][j];
+        dfs(prefix, grid, copy, i - 1, j, words);
+        dfs(prefix, grid, copy,i + 1, j, words);
+        dfs(prefix, grid, copy, i, j - 1, words);
+        dfs(prefix, grid, copy, i, j + 1, words);
+        copy[i][j] = false;
     }
 }
